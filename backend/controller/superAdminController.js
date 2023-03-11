27b20f51cda1fa68f591b6a8e35e5a2d34adf2f8
GIHAA <gihansudeeparandimal@gmail.com>
2023-03-11 15:03:16 +0530

    Added super admin CRUD

---
+++ backend/controller/superAdminController.js
@@ -0,0 +1,97 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const addHealthPro = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+      role: user.role,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+const updateHealthPro = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ _id: req.body._id });
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.role = req.body.role || user.role;
+    user.password = req.body.password || user.password;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser.id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      token: generateToken(updatedUser._id),
+      role: updatedUser.role,
+    });
+  }
+});
+const deleteHealthPro = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const deletedUser = await user.delete();
+    res.json({
+      _id: deletedUser.id,
+      name: deletedUser.name,
+      email: deletedUser.email,
+      token: generateToken(deletedUser._id),
+      role: deletedUser.role,
+      message: "User deleted",
+    });
+  } else {
+    res.status(400);
+    throw new Error("delete failed");
+  }
+});
+
+module.exports = {
+  addHealthPro,
+  updateHealthPro,
+  deleteHealthPro,
+};
--- backend/routes/superAdmin.js
+++ backend/routes/superAdmin.js
@@ -9,6 +9,13 @@ const {
   getMe,
   updateUser,
 } = require('../controller/userController')
+
+const {
+    addHealthPro,
+    updateHealthPro,
+    deleteHealthPro,
+
+} = require('../controller/superAdminController')
 const { protect ,  patientProtect , healthProProtect , superAdminProtect} = require('../middleware/authMiddleware')
 
 
@@ -16,6 +23,9 @@ router.post('/',registerUser)
 router.post('/login', loginUser)
 router.post('/update',protect, updateUser)
 router.get('/me', protect, getMe)
+
+router.post('/healthpro', addHealthPro)
+router.post('/healthpro', addHealthPro)
 
 
 module.exports = router
